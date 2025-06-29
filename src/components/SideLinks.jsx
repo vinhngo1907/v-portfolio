bbe9d5a40dc366fcb94358f157c01462c8e20853
Koo Kuu <vinhtrungngo1907@gmail.com>
2025-06-29 10:55:04 +0700

    commit

--- src/components/SideLinks.jsx
+++ src/components/SideLinks.jsx
@@ -10,13 +10,15 @@ export default function SideLinks() {
                 <li className={"py-2"}><a href={"https://linkedin.com/in/henry1907"} target={"_blank"} className={"hover-accent hover-displace fab fa-linkedin"} rel="noreferrer" /></li>
                 {/* eslint-disable-next-line jsx-a11y/anchor-has-content */}
                 <li className={"py-2 pb-4"}><a href={"https://www.quora.com/profile/Henry-Ngo-89"} target={"_blank"} className={"hover-accent hover-displace fab fa-quora"} rel="noreferrer" /></li>
+                {/* eslint-disable-next-line jsx-a11y/anchor-has-content */}
+                <li className={"py-2 pb-4"}><a href={"https://leetcode.com/u/vinhngo1907"} target={"_blank"} className={"hover-accent hover-displace fab fa-codepen"} rel="noreferrer" /></li>
             </ul>
             <div className={"slate hidden xl:flex flex-col fixed bottom-0 right-0 items-center px-14 after-line w-fit"}>
                 <a href={"mailto:vinhtrungngo1907@gmail.com"} 
                     className={"flex flex-col items-center hover-accent pb-4 hover-displace cursor-pointer font-monospace font-medium tracking-widest"}>
                     {/* <span className={"pb-2 text-xl"}>@</span> */}
                     <span className={"vertical-text opacity-60 text-sm"}>
-                        vinhtrungngo1907@gmail.com
+                        henryngo1907@gmail.com
                     </span>
                 </a>
             </div>
