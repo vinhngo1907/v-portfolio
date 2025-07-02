d8675f492bb27a0eb6f37e2084bcf0a99927e182
Koo Kuu <vinhtrungngo1907@gmail.com>
2025-07-02 08:52:53 +0700

    commit

--- src/views/Work.jsx
+++ src/views/Work.jsx
@@ -7,7 +7,7 @@ import XKA from "../images/projects/XKA.png";
 import NguyenVuStore from "../images/projects/NguyenVu.png";
 import FeaturedProject from "../components/FeaturedProjects";
 import Project from "../components/Projects";
-import {useState} from "react";
+import { useState } from "react";
 
 export default function Work() {
 
@@ -15,19 +15,26 @@ export default function Work() {
         {
             name: "V Network - Social Media",
             description: "An interactive social media platform with real-time messaging, voice/video calls, post creation, reactions, user search, notifications, group chats, stories, nearby people, and boosted ads. Supports basic login and OAuth2.",
-            tags: ["NodeJS", "Express", "Mongo", "React", "Bootstrap"],
+            tags: ["NodeJS", "Google API", "Facebook API", "Twilio", "Mongo", "React"],
             links: {
-                "fab fa-github": "https://github.com/vinhngo1907/mern-v-social-media", 
+                "fab fa-github": "https://github.com/vinhngo1907/mern-v-social-media",
                 "fas fa-external-link": "#"
             },
             image: VNetwork
         },
         {
+            name: "FM - CMS",
+            description: "This website enables remote control of local speakers. It also provides customers with user access management and allows them to easily monitor and manage connected devices in real time across different areas.",
+            tags: ["Nest", "Mongo", "Socket.io", "PosgreSQL", "Next", "Prisma", "Strapi", "Grafana"],
+            links: { "fab fa-github": "#", "fas fa-external-link": "https://dev-fe.hoangthelongtto.vn/" },
+            image: FmCMS
+        },
+        {
             name: "V Music – Listen & Chill",
-            description: "A web application for seamless music streaming and management. Users can browse, play, and organize tracks, manage playlists, and react with likes or dislikes.",  
-            tags: ["NodeJS", "Express","YouTube Data API", "Mongo"],
+            description: "A web application for seamless music streaming and management. Users can browse, play, and organize tracks, manage playlists, and react with likes or dislikes.",
+            tags: ["NodeJS", "Express", "YouTube Data API", "Mongo"],
             links: {
-                "fab fa-github": "https://github.com/vinhngo1907/v-music-stream", 
+                "fab fa-github": "https://github.com/vinhngo1907/v-music-stream",
                 "fas fa-external-link": "https://v-music-stream.onrender.com"
             },
             image: MusicStream
@@ -35,30 +42,25 @@ export default function Work() {
         {
             name: "Hasu Farm",
             description: "HASU Farm is a smart agriculture platform that monitors and manages IoT farm data such as temperature, humidity, and lighting in real time. The system collects sensor data, processes it through a scalable backend, and provides farmers with an intuitive dashboard for tracking and controlling their farm environment efficiently.",
-            tags: ["NodeJS", "Mongo", "MQTT", "React", "Arduino"],
-            links: {"fab fa-github": "#",
-            "fas fa-external-link": "http://hasufarm.com"},
+            tags: ["NodeJS", "Express", "Mongo", "MQTT", "React", "Arduino"],
+            links: {
+                "fab fa-github": "#",
+                "fas fa-external-link": "http://hasufarm.com"
+            },
             image: HasuFarm
-        },
-        {
-            name: "FM - CMS",
-            description: "This website enables remote control of local speakers. It also provides customers with user access management and allows them to easily monitor and manage connected devices in real time across different areas.",
-            tags: ["Nest","Mongo", "Socket.io", "PosgreSQL", "Next", "Prisma", "Strapi"],
-            links: {"fab fa-github": "#","fas fa-external-link": "https://dev-fe.hoangthelongtto.vn/"},
-            image: FmCMS
         },
         {
             name: "XKA - ECommerce Website",
             description: "An e-commerce platform designed to help small and local sports equipment shops easily transition to online business.",
             tags: ["Wordpress", "Next", "Cloudinary", "MySQL", "Docker", "Traefik"],
-            links: {"fab fa-github": "#","fas fa-external-link": "https://xka.vn/"},
+            links: { "fab fa-github": "#", "fas fa-external-link": "https://xka.vn/" },
             image: XKA
         },
         {
             name: "Nguyen Vu Store",
             description: "Nguyen Vu Store's e-commerce website for online sales provides all the basic functions of an e-commerce website, including online payment.",
             tags: ["Wordpress", "Next", "Cloudinary", "MySQL", "Docker", "Traefik"],
-            links: {"fab fa-github": "#","fas fa-external-link": "https://nguyenvu.store"},
+            links: { "fab fa-github": "#", "fas fa-external-link": "https://nguyenvu.store" },
             image: NguyenVuStore
         }
     ])
@@ -94,16 +96,16 @@ export default function Work() {
                         title={" V Note App"}
                         description={"Take notes and organize them in folders with real-time updates."}
                         links={{
-                            "fab fa-github": "#", 
+                            "fab fa-github": "#",
                             "fas fa-external-link": "https://v-note-app.netlify.app"
                         }}
-                        tags={["Apollo GraphQL",  "NodeJS", "Mongo", "React", "Firebase"]}
+                        tags={["Apollo GraphQL", "NodeJS", "Mongo", "React", "Firebase"]}
                     />
                     <Project
                         title={"Chat Application"}
                         description={"A real-time platform for private chat, calls, and live presence."}
                         links={{
-                            "fab fa-github": "https://github.com/vinhngo1907/mern-v-chat-app", 
+                            "fab fa-github": "https://github.com/vinhngo1907/mern-v-chat-app",
                             "fas fa-external-link": "https://mern-v-chat-app.netlify.app/"
                         }}
                         tags={["React", "NodeJS", "Mongo", "Cloudinary", "Socket", "Redis"]}
@@ -111,13 +113,13 @@ export default function Work() {
                     <Project
                         title={"Course Manager"}
                         description={"V-Course system which allows you manage your courses"}
-                        links={{"fab fa-github": "https://github.com/vinhngo1907/v-course-manager"}}
+                        links={{ "fab fa-github": "https://github.com/vinhngo1907/v-course-manager" }}
                         tags={["Next", "Nest", "Postgres", "MinIO", "Prisma", "Postgres"]}
                     />
                     <Project
                         title={"V Cursor Application"}
                         description={"A small chat system based on a microservices architecture"}
-                        links={{"fab fa-github": "https://github.com/vinhngo1907/v-cursor"}}
+                        links={{ "fab fa-github": "https://github.com/vinhngo1907/v-cursor" }}
                         tags={["Vue", "Nest", "Mongo", "Kafka", "Python"]}
                     />
                     <Project
@@ -125,17 +127,17 @@ export default function Work() {
                         description={
                             "An app for uploading and sharing files via unique download links."
                         }
-                        links={{"fab fa-github": "https://github.com/vinhngo1907/sevn-inshare-app"}}
+                        links={{ "fab fa-github": "https://github.com/vinhngo1907/sevn-inshare-app" }}
                         tags={["Vue", "NodeJS", "Sequelize", "MySQL"]}
                     />
                     <Project
                         title={" Hoang Long OHayO"}
                         description={" A Booking system for hotel, coffee, restaurant, and conference."}
                         links={{
-                            "fab fa-github": "#", 
+                            "fab fa-github": "#",
                             "fas fa-external-link": "https://hoanglonghotel.com/"
                         }}
-                        tags={["Strapi",  "Postgres", "Prisma", "FullterFlow", "Next"]}
+                        tags={["Strapi", "Postgres", "Prisma", "FullterFlow", "Next"]}
                     />
                 </div>
             </div>
